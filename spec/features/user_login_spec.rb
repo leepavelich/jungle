79c672e50b26d0b347b11ae0fc7f887917c50750
Lee Pavelich <leepavelich@gmail.com>
2022-03-16 01:03:14 +0000

    Testing that user login is successful (already done in the add to cart spec but oh well

---
+++ spec/features/user_login_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+RSpec.feature "User login", type: :feature, js: true do
+
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    @user = User.create do |u|
+      u.email = "test@testmail.com"
+      u.first_name = "First"
+      u.last_name = "Last"
+      u.password = "password"
+      u.password_confirmation = "password"
+    end
+
+    3.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "The user adds a product to the cart" do
+    visit '/login'
+    within('form') do
+      fill_in 'email', :with => 'test@testmail.com'
+      fill_in 'password', :with => 'password'
+    end
+    click_button 'Submit'
+
+    visit root_path
+
+    expect(page).to have_content("First Last")
+
+    save_screenshot
+  end
+end
